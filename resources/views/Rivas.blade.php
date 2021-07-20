9c7f1387d702e640d991515a1d416cf11dd31625
RusticDeveloper <rivasdaniel325@gmail.com>
2021-07-20 14:18:19 -0500

    Added a file with WoW info

---
+++ resources/views/Rivas.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Presentación</title>
+</head>
+<body>
+    <h1>👉👉WoW👈👈</h1>
+    <blockquote>World of Warcraft takes place within the world of Azeroth, approximately
+         four years after the events of the previous game in the series, Warcraft III: 
+         The Frozen Throne.</blockquote>
+</body>
+</html>
