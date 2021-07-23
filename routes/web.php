4189ecc6b13fa61150f696439f2d4d6828610520
RusticDeveloper <rivasdaniel325@gmail.com>
2021-07-23 18:34:34 -0500

    update on routing 23/07/2021

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('Rivas');
+});
+
+Route::get('players/{playerID}/games', function () {
+    return ['COD Modern Warfare', 'Horizon Zero Dawn','Warhammer 4000'];
+});
+Route::get('players/{playerID}/games/{gameID}', function () {
+    return 'KoF 2003';
+});
+Route::post('players/{playerID}/games', function () {
+    return 'fetching games list';
+});
+Route::put('players/{playerID}/games/{gameID}', function () {
+    return 'game updated!!';
+});
+Route::delete('players/{playerID}/games/{gameID}', function () {
+    return 'game deleted your set';
 });
